55e25b230ff9e3cf2ef05331131e3a754bd2c562
ChirtoacaLilia <159546649+ChirtoacaLilia@users.noreply.github.com>
2024-04-23 18:39:28 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,40 @@
+new SimpleLightbox({elements:".galerie-container a"})
+
+new SimpleLightbox({elements:".ora-imagini a"})
+
+ AOS.init({ once: true });
+
+emailjs.init("eNDsXusoxoA_tJ-Hw");
+
+function mobileMenu() {
+    var x = document.getElementById("navbar");
+
+    if (x.className === "") {
+        x.className = "mobile";
+    } else {
+        x.className = "";
+    }
+}
+
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send("service_vsiybmd", "template_3nrzfk8", date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+
+}
